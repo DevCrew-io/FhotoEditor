33e0ec8e3cb83acd8313e7a84b539cd89d560463
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-03-13 16:28:28 +0500

    wip: implement draggable rectangle for selecting the cropping area

---
+++ lib/image_crop.dart
@@ -0,0 +1,13 @@
+library image_crop;
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'dart:ui' as ui;
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+
+part 'features/cropping/image_crop.dart';
+part 'features/cropping/crop.dart';
