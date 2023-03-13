f6aa2879bfacfdb125858c2ee71e893cdd17c5fa
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-03-13 17:41:39 +0500

    feat: implement cropping feature in native side of android

--- lib/image_crop.dart
+++ lib/image_crop.dart
@@ -8,6 +8,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
+import 'const.dart';
 
 part 'features/cropping/image_crop.dart';
 part 'features/cropping/crop.dart';
