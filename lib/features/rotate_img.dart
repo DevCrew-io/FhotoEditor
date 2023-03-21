3884f75f79f685f89aa2ebf8a82a3cfcc2cbc6c9
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-03-21 14:57:21 +0500

    feat: implement rotation on image

---
+++ lib/features/rotate_img.dart
@@ -0,0 +1,23 @@
+part of image_crop;
+
+/// Rotate given Image
+
+Widget rotate(Image img, double rotation){
+  return  Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.rotationZ(
+        rotation,
+      ),
+      child: img
+  );
+}
+
+/// set math.pi 0r 0 for 180 degree horizontal/vertical flip
+
+Widget flipImage(Image img, double rotation,bool isVertical){
+  return Transform(
+    alignment: Alignment.center,
+    transform:isVertical? Matrix4.rotationY(rotation): Matrix4.rotationX(rotation),
+    child: img,
+  );
+}
--- lib/image_crop.dart
+++ lib/image_crop.dart
@@ -10,7 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 import 'const.dart';
-import 'dart:math';
+import 'dart:math' as math; // import this
 
 
 part 'features/cropping/image_crop.dart';
@@ -19,4 +19,5 @@ part 'features/cropping/image_crop_impl.dart';
 part 'features/adjust/color_filter_generator.dart';
 part 'features/adjust/color_filter_gen_impl.dart';
 part 'features/adjust/image_filter.dart';
+part 'features/rotate_img.dart';
 
