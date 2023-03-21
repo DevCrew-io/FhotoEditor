676bda44b379d6def3a5620d7c665a902c305b37
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-03-21 14:20:04 +0500

    feat: implement example flip image vertical and horizontal

---
+++ example/lib/features/rotate/flip_image.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+import 'package:fhoto_editor/image_crop.dart';
+import 'package:flutter/material.dart';
+
+class FlipImage extends StatefulWidget {
+  const FlipImage({Key? key}) : super(key: key);
+
+  @override
+  State<FlipImage> createState() => _FlipImageState();
+}
+
+class _FlipImageState extends State<FlipImage> {
+  double _rotation = 0.0;
+  bool _isVerticalFlip = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                flipHorizontal(
+                    Image.network("https://www.gstatic.com/webp/gallery/1.jpg"),
+                    _rotation,
+                    _isVerticalFlip),
+                const Expanded(
+                  child: Text("Bottom"),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          _isVerticalFlip = true;
+                          setState(() {
+                            if (_rotation == 0.0) {
+                              _rotation = pi;
+                            } else {
+                              _rotation = 0.0;
+                            }
+                          });
+                        },
+                        child: const Text("Flip Horizontal")),
+                    TextButton(
+                        onPressed: () {
+                          _isVerticalFlip = false;
+                          setState(() {
+                            if (_rotation == 0.0) {
+                              _rotation = pi;
+                            } else {
+                              _rotation = 0.0;
+                            }
+                          });
+                        },
+                        child: const Text("Flip Vertical"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
