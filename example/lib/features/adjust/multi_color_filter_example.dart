cf0e5e068298e72263c3a2d715059e86e7d40b24
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-07-04 12:10:37 +0500

    feat: use all photoshop features in sample

--- example/lib/features/adjust/multi_color_filter_example.dart
+++ example/lib/features/adjust/multi_color_filter_example.dart
@@ -1,5 +1,9 @@
 import 'package:fhoto_editor/fhoto_editor.dart';
+import 'package:fhoto_editor_example/features/cropping/cropping.dart';
+import 'package:fhoto_editor_example/features/transformation/flip.dart';
 import 'package:flutter/material.dart';
+
+import '../transformation/rotate.dart';
 
 class MultiColorFilterExample extends StatefulWidget {
   const MultiColorFilterExample({Key? key}) : super(key: key);
@@ -16,6 +20,19 @@ class _MultiColorFilterExampleState extends State<MultiColorFilterExample> {
   }
 
   double _brightnessValue = 0;
+  double _hueSeekbarValue = 0;
+  double _contrastSeekbarValue = 0;
+  double _saturationSeekbarValue = 0;
+  double _exposureSeekbarValue = 0;
+  double _shadowSeekbarValue = 0;
+  double _highlightedSeekbarValue = 0;
+  double _fadedSeekbarValue = 1;
+  double _vibrancySeekbarValue = 0;
+  double _temperatureSeekbarValue = 0;
+
+  bool _isCropClicked = false;
+  bool _isRotateClicked = false;
+  bool _isFlip = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +41,16 @@ class _MultiColorFilterExampleState extends State<MultiColorFilterExample> {
     /// Apply multi filter on Image
 
     ColorMultiFilterGenerator myFilter = ColorMultiFilterGenerator(filters: [
-      colorGen.getContrastMatrix(value: .5),
-      colorGen.getBrightnessMatrix(value: _brightnessValue / 20),
+      colorGen.getHueMatrix(value: _hueSeekbarValue),
+      colorGen.getContrastMatrix(value: _contrastSeekbarValue),
+      colorGen.getBrightnessMatrix(value: _brightnessValue),
+      colorGen.getSaturationMatrix(value: _saturationSeekbarValue),
+      colorGen.getExposureMatrix(value: _exposureSeekbarValue),
+      colorGen.getShadowMatrix(value: _shadowSeekbarValue),
+      colorGen.getHighlightedMatrix(value: _highlightedSeekbarValue),
+      colorGen.getFadedMatrix(value: _fadedSeekbarValue),
+      colorGen.getVibrancyMatrix(value: _vibrancySeekbarValue),
+      colorGen.getTemperatureMatrix(value: _temperatureSeekbarValue),
     ]);
 
     return MaterialApp(
@@ -33,29 +58,227 @@ class _MultiColorFilterExampleState extends State<MultiColorFilterExample> {
         appBar: AppBar(
           title: const Text('Color Filter Example'),
         ),
-        body: Stack(
-          children: [
-            Center(
-                child: ColorFiltered(
-                    colorFilter: ColorFilter.matrix(myFilter.matrix),
-                    child: Image.asset('assets/shahid.jpeg'))),
-            Column(
-              children: [
-                Slider(
-                  divisions: 100,
-                  label: 'Brightness',
-                  onChanged: (value) {
-                    setState(() {
-                      _brightnessValue = value;
-                    });
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                  child: ColorFiltered(
+                      colorFilter: ColorFilter.matrix(myFilter.matrix),
+                      child: Image.asset('assets/shahid.jpeg'))),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Hue"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Hue',
+                        onChanged: (value) {
+                          setState(() {
+                            _hueSeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _hueSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Contrast"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Contrast',
+                        onChanged: (value) {
+                          setState(() {
+                            _contrastSeekbarValue = value;
+                            // _contrastSeekbarValue=_contrastSeekbarValue/100;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _contrastSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Brightness"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Brightness',
+                        onChanged: (value) {
+                          setState(() {
+                            _brightnessValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _brightnessValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Saturation"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Saturation',
+                        onChanged: (value) {
+                          setState(() {
+                            _saturationSeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _saturationSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Exposure"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Exposure',
+                        onChanged: (value) {
+                          setState(() {
+                            _exposureSeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _exposureSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Shadow"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Shadow',
+                        onChanged: (value) {
+                          setState(() {
+                            _shadowSeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _shadowSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Highlighted"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Highlighted',
+                        onChanged: (value) {
+                          setState(() {
+                            _highlightedSeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _highlightedSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Faded"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Faded',
+                        onChanged: (value) {
+                          setState(() {
+                            _fadedSeekbarValue = value;
+                          });
+                        },
+                        max: 10,
+                        min: 0,
+                        value: _fadedSeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Vibrancy"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Vibrancy',
+                        onChanged: (value) {
+                          setState(() {
+                            _vibrancySeekbarValue = value;
+                          });
+                        },
+                        max: 1,
+                        min: 0,
+                        value: _vibrancySeekbarValue,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text("Temperature"),
+                      Slider(
+                        divisions: 100,
+                        label: 'Temperature',
+                        onChanged: (value) {
+                          setState(() {
+                            _temperatureSeekbarValue = value;
+                          });
+                        },
+                        max: 10,
+                        min: -5,
+                        value: _temperatureSeekbarValue,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RotateImage()),
+                    );
                   },
-                  max: 20,
-                  min: 0,
-                  value: _brightnessValue,
-                ),
-              ],
-            )
-          ],
+                  child: const Text("Rotate Image")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const FlipImage()),
+                    );
+                  },
+                  child: const Text("Flip Image")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Cropping()),
+                    );
+                  },
+                  child: const Text("Crop Image"))
+            ],
+          ),
         ),
       ),
     );
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:fhoto_editor_example/features/adjust/multi_color_filter_example.dart';
 import 'package:flutter/material.dart';
 import 'features/adjust/color_filter_example.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ColorFilterExample(),
+      home: MultiColorFilterExample(),
     ),
   );
 }
