fc8c844b4eed93e6e852c29689c5de4411fb1e1c
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-07-07 16:29:12 +0500

    feat: add reporting Potential Security Issues file

---
+++ SECURITY.md.dart
@@ -0,0 +1,30 @@
+# Security
+
+## Reporting Potential Security Issues
+
+If you discover a potential security vulnerability in this project,
+please notify us immediately by sending an email to arslan.khan@devcrew.io.
+We appreciate your cooperation in helping us maintain the security and
+integrity of our project.
+
+When reporting issues, please provide the following information:
+
+- Specify the affected component(s).
+- Describe the steps to reproduce the issue.
+- Summarize the security vulnerability and its potential impact.
+
+To protect our users and allow us to address the vulnerability before it
+becomes public knowledge, we kindly request that you contact us through
+the provided email address. This will enable us to release patches and updates
+to safeguard our users' applications.
+
+## Policy
+
+Once a reported security vulnerability is verified, our policy is as follows:
+
+- We will promptly patch the current release branch and the immediate prior minor release branch.
+
+- We will release new security fix updates for each patched release branch without delay.
+
+Thank you for your collaboration in maintaining the security of our project and for helping us
+create a safer environment for our users.
